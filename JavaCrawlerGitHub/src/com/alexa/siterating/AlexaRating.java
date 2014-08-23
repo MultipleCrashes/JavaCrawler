cdb5008ba0826ae07baf30ebfffa767325b1b77f
Harish <inimitableharish@gmail.com>
2014-08-24 05:29:04 +0530

    Alexa rating first commit

--- JavaCrawlerGitHub/src/com/alexa/siterating/AlexaRating.java
+++ JavaCrawlerGitHub/src/com/alexa/siterating/AlexaRating.java
@@ -6,10 +6,53 @@
 
 package com.alexa.siterating;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import javax.lang.model.element.Element;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
 /**
  *
  * @author ubuntu
  */
 public class AlexaRating {
+    
+    public static void main getAlexaRating(String args[]) throws IOException
+    {
+        int result=0;
+        String domain="http://www.google.com";
+        String url="http://data.alex.com/data?cli=10&url="+domain;
+        
+        try 
+        {
+            URLConnection conn=new URL(url).openConnection();
+            InputStream is=conn.getInputStream();
+            DocumentBuilder dBuilder=DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc=dBuilder.parse(is);
+            Element element=doc.getDocumentElement();
+            NodeList nodeList=element.getElementsByTagName("POPULARITY");
+            if(nodeList.getLength()>0)
+            {
+                Element elementAttribute=(Element) nodeList.item(0);
+                String ranking= elementAttribute.getAttribute("TEXT");
+                if(!"".equals(ranking))
+                {
+                    result=Integer.valueOf(ranking);
+                }
+            }
+            
+        }
+        catch(Exception e)
+                {
+                    System.out.println(e.getMessage());
+                }
+        System.out.print("The rating is ");
+        System.out.println(result);
+    }
     
 }
