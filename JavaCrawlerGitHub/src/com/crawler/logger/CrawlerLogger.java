68931179fade60f0c2b4a5a7529af61d79246770
Harish <inimitableharish@gmail.com>
2014-08-24 04:25:31 +0530

    Enabling logging basic and introducing crawler configuration

---
+++ JavaCrawlerGitHub/src/com/crawler/logger/CrawlerLogger.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.crawler.logger;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ubuntu
+ */
+public class CrawlerLogger {
+    public void logger(String args[]) throws IOException
+    {
+        
+    }
+    
+}
